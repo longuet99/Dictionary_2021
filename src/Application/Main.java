30203bbc50967ac94dd6b578f0609ab440461c64
longuet99 <rongconblabla@gmail.com>
2021-10-27 03:09:38 +0700

    final_final Dictionary_2021

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -8,6 +8,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.File;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
 
 public class Main extends Application {
 
@@ -18,6 +22,20 @@ public class Main extends Application {
             Parent root = loader.load();
 
             Controller controller = loader.getController();
+
+//            new AnimationTimer() {
+//                public void handle(long currentNanoTime) {
+//                    List<String> list = null;
+//                    try {
+//                        list = controller.actions.realtimeSearch(controller.getWord());
+//                    } catch (SQLException throwables) {
+//                        throwables.printStackTrace();
+//                    }
+//                    ObservableList<String> observableList = FXCollections.observableArrayList(list);
+//                    controller.getListview().setItems(observableList);
+//                }
+//            }.start();
+
             Scene searchScene = new Scene(root);
 
             primaryStage.setTitle("DICTIONARY - 2021");
@@ -28,6 +46,7 @@ public class Main extends Application {
 
         }
     }
+
 
     public static void main(String[] args) {
         launch(args);
